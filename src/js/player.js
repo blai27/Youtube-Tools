5dcb3b9e8dbed2a919e99f650438d4665e798e9b
Ben <blai27wd@gmail.com>
2014-10-27 20:59:09 -0400

    File restructure. Hopefully indentations are fixed.

---
+++ src/js/player.js
@@ -0,0 +1,17 @@
+var player = null;
+function getAllOptions() {
+	console.log(player.getOptions());
+	console.log(player.getOptions('iv-module'));
+}
+function onYouTubePlayerReady(playerId) {
+	player = document.getElementById('movie_player');
+	console.log(player);
+	player.setVolume(75);
+	console.log(player.getDuration());
+	console.log(player.getAvailableQualityLevels());
+	player.setPlaybackQuality('medium');
+	console.log('API call success');
+	console.log(player.getVideoLoadedFraction());
+	player.addEventListener('onApiChange', 'getAllOptions');
+}
+console.log("injected successfully");
