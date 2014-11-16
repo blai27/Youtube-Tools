2f276a8dbcdfb7b54445df3f3760eb46763b18f4
Ben <blai27wd@gmail.com>
2014-11-16 17:49:41 -0500

    Feature commit: removing the automatic channel wide playlist attached when selecting/opening videos from a channel's video tab.

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,33 @@
 (function(){
+  
+  /* Declarations for all URL regex matching patterns */
 
-  var bgSettings = null;
+  var YT_URLs = {
+    nav: {
+      channel_video_list: /.+:\/\/www\.youtube\.com\/user\/.*\/videos/
+    },
+    player: {
+
+    }
+  };
+
+  var msg_params = {};
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo && changeInfo.status === 'loading') {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.storage.sync.get('nav_rmPL', function(result){
+          var rmPLtoggle = result['nav_rmPL'];
+          if (rmPLtoggle) {
+            if (YT_URLs.nav.channel_video_list.test(tab.url)) {
+              msg_params = {nav_rmPL: true};    
+              chrome.tabs.sendMessage(tabs[0].id, msg_params, function(response) {
+                // TODO: may be useful later on with a response handler
+              });      
+            }
+          }
+        });
+      }); 
+    }
+  });
 })();
--- src/js/playlist_removal.js
+++ src/js/playlist_removal.js
@@ -1,37 +1,105 @@
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   console.log("Injected successfully");
-// });
-
 (function(){
+  
+  var removedTag = 'list-href-removed';
+  var content = '#content';
+  var gridContainer = '#channels-browse-content-grid';
+  var listContainer = '#browse-items-primary';
+  var imgHrefTile = '.ux-thumb-wrap';
+  var imgHrefRemoved = '.ux-thumb-wrap:not(.list-href-removed)';
+  var titleHrefLink = '.yt-uix-tile-link';
+  var titleHrefRemoved = '.yt-uix-tile-link:not(.list-href-removed)';
+  var listInsertTarget = 'li.feed-item-container';
+  var gridInsertTarget = 'li.channels-content-item';
+  var matchURL = /.+:\/\/www\.youtube\.com\/user\/.*\/videos/;
 
   function removePlaylistHref(ytTile) {
     var hrefURL = ytTile.attr('href');
     var tokens = hrefURL.split(/&list/);
     console.log(hrefURL);
     console.log(tokens[0]);
-    ytTile.addClass('list-href-removed');
-    ytTile.attr('href', tokens[0]);    
+    ytTile.addClass(removedTag);
+    ytTile.attr('href', tokens[0]); 
   }
 
-  // Disable auto list link from channel/video -> viewer
-  $('.yt-uix-tile-link').each(function(){
-    removePlaylistHref($(this));
-  });
+  // Attempt to find un-removed links with cached pages.
+  // When the user clicks the back/forward action in browser, cached pages are loaded instanteniously.
+  // Can only catch this scenario with a reasonable timeout.
+  // TODO: Add browser action listener in background page and send a message to here whenever user goes forward or backward
 
-  $('a.ux-thumb-wrap').each(function(){
-    removePlaylistHref($(this));
-  });
+  function cachedExecute() {
+    setTimeout(function() {
+      if ($(imgHrefRemoved).length) {
+        init();
+      }
+    }, 1000);
+  }
 
-  // Bind click event to Load More button, listen for the videos container mutation event
-  // and perform the same href list removal
-  $('.yt-uix-load-more').click(function(){
-    $('#channels-browse-content-grid').bind('DOMSubtreeModified', function(){
-      $('.yt-uix-tile-link:not(.list-href-removed)').each(function(){
-        removePlaylistHref($(this));
-      });
-      $('.ux-thumb-wrap:not(.list-href-remove)').each(function(){
-        removePlaylistHref($(this));
-      });
+  function initSubsequentCallListener() {
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.nav_rmPL) {
+        $(document).off().on('DOMNodeInserted', content, function(event) {
+
+          if (($(event.target).find(gridContainer).length
+                || $(event.target).find(listContainer).length) && matchURL.test(event.target.baseURI)) {
+            $(document).off();
+            removeListeners();
+            init();
+          }
+
+          // If we catch one insertion event and the URL does not match, then disable all event handlers
+          else if (!matchURL.test(event.target.baseURI)) {
+            $(document).off();
+          }
+        });
+        cachedExecute();
+      }
     });
+  }
+
+  function execute(newTitles, newImgs, isGrid) {
+    if (newTitles.length) {
+      newTitles.each(function() {
+        removePlaylistHref($(this));
+      });
+    }
+    if (newImgs.length) {
+      newImgs.each(function() {
+        removePlaylistHref($(this));
+      });
+    }
+  }
+
+  function removeListeners() {
+    $(gridContainer).off();
+    $(listContainer).off();
+  }
+
+  function init() {
+    execute($(titleHrefLink), $(imgHrefTile));
+
+    $(gridContainer).on('DOMNodeInserted', gridInsertTarget,function(event) {
+      execute($(event.target).find(titleHrefRemoved),
+              $(event.target).find(imgHrefRemoved), 'grid');
+    });
+
+    $(listContainer).on('DOMNodeInserted', listInsertTarget, function(event) {
+      execute($(event.target).find(titleHrefRemoved),
+              $(event.target).find(imgHrefRemoved), 'list');
+    });
+  }
+
+  removeListeners();
+
+  // This URL matching test is for testing if the first Youtube site loaded is within any channel.
+  // Since this content script is injected in any Youtube page (due to AJAX modifications on DOM + HTML5 pushState usage by Youtube pages),
+  // We need to make sure that this script is ran immediately when the current tab lands on a [channel+videos] page
+
+  chrome.storage.sync.get('nav_rmPL', function(result){
+    var rmPLtoggle = result['nav_rmPL'];
+    if (rmPLtoggle && matchURL.test(window.location.href)) {
+      init();
+    }
+    initSubsequentCallListener();
   });
+
 })();
